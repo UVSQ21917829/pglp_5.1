6d2cc7caee9554e44e444dc4aa8c3280274370e6
ait ihya <hajaraitihya@gmail.com>
2020-03-24 13:26:12 +0100

    pom configuration

---
+++ exercice5_4/src/main/java/fr/uvsq/exercice5_4/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq.exercice5_4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
