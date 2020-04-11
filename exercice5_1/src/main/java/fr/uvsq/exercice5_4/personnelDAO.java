20c11573eec9586872a0e69280dea486e9cdbd04
ait ihya hajar <hajar.ait-ihya@ens.uvsq.fr>
2020-04-11 18:47:15 +0200

    personnemDaO

---
+++ exercice5_1/src/main/java/fr/uvsq/exercice5_4/personnelDAO.java
@@ -0,0 +1,25 @@
+package fr.uvsq.exercice5_4;
+
+public class personnelDAO implements DAO<Personnel> {
+
+	public Personnel create(Personnel obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Personnel read(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Personnel update(Personnel obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Personnel delete(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
