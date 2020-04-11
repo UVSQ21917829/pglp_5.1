0e5de861c5a00502a5828870210fa02b6ac7f856
ait ihya hajar <hajar.ait-ihya@ens.uvsq.fr>
2020-04-11 20:53:01 +0200

    testRead pour groupeDAO

--- exercice5_1/src/test/java/fr/uvsq/exercice5_4/TestGroupDao.java
+++ exercice5_1/src/test/java/fr/uvsq/exercice5_4/TestGroupDao.java
@@ -12,13 +12,26 @@ public class TestGroupDao {
 
 	@Test
 	public void testCreat() throws IOException {
+		
 		GroupeDAO goupdao = new GroupeDAO();
 		Personnel p1= new Personnel.Builder(13, "aa", "jj",LocalDate.parse("1997-08-01",DateTimeFormatter.ISO_DATE)).build();
 		CompositePersonnels groupe= new CompositePersonnels(1);
 		groupe.addPersonnel(p1);
 		goupdao.create(groupe);
+			
+	}
+	@Test
+	public void testRead() throws IOException, ClassNotFoundException {
 		
-		
+		GroupeDAO goupdao = new GroupeDAO();
+		CompositePersonnels p;
+		Personnel p1= new Personnel.Builder(13, "aa", "jj",LocalDate.parse("1997-08-01",DateTimeFormatter.ISO_DATE)).build();
+		CompositePersonnels groupe= new CompositePersonnels(2);
+		groupe.addPersonnel(p1);
+		goupdao.create(groupe);
+		p=goupdao.read(2);
+		assertTrue(p.getId()==2);
+			
 	}
 
 }
