ed995ae623f19007da1cc6439901e5893dbb100e
ait ihya hajar <hajar.ait-ihya@ens.uvsq.fr>
2020-04-11 18:47:27 +0200

    groupe dao

---
+++ exercice5_1/src/main/java/fr/uvsq/exercice5_4/GroupeDAO.java
@@ -0,0 +1,25 @@
+package fr.uvsq.exercice5_4;
+
+public class GroupeDAO implements DAO<CompositePersonnels> {
+
+	public CompositePersonnels create(CompositePersonnels obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public CompositePersonnels read(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public CompositePersonnels update(CompositePersonnels obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public CompositePersonnels delete(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
