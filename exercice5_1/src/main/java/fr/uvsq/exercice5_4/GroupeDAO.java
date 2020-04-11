3b557857cbd9ae97903a30599386f544252cc1d9
ait ihya hajar <hajar.ait-ihya@ens.uvsq.fr>
2020-04-11 20:49:14 +0200

    groupeDao imlementation

--- exercice5_1/src/main/java/fr/uvsq/exercice5_4/GroupeDAO.java
+++ exercice5_1/src/main/java/fr/uvsq/exercice5_4/GroupeDAO.java
@@ -1,20 +1,41 @@
 package fr.uvsq.exercice5_4;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 public class GroupeDAO implements DAO<CompositePersonnels> {
 
-	public CompositePersonnels create(CompositePersonnels obj) {
+	public CompositePersonnels create(CompositePersonnels obj) throws IOException {
 		// TODO Auto-generated method stub
-		return null;
+		
+		FileOutputStream fout = new FileOutputStream("groupe.ser") ;
+    	ObjectOutputStream out = new ObjectOutputStream(fout) ;
+    	out.writeObject(obj) ; 
+    	out.close() ;
+		return obj;
 	}
 
-	public CompositePersonnels read(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	public CompositePersonnels read(Integer id) throws IOException, ClassNotFoundException {
+		CompositePersonnels groupe;
+		FileInputStream fileInput = new FileInputStream("groupe.ser");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInput);
+        
+        groupe = (CompositePersonnels) objectInputStream.readObject();
+  
+		return groupe;
 	}
 
-	public CompositePersonnels update(CompositePersonnels obj) {
+	public CompositePersonnels update(CompositePersonnels obj) throws IOException {
 		// TODO Auto-generated method stub
-		return null;
+		
+		FileOutputStream fout = new FileOutputStream("groupe.ser") ;
+    	ObjectOutputStream out = new ObjectOutputStream(fout) ;
+    	out.writeObject(obj) ; 
+    	out.close() ;
+		return obj;
 	}
 
 	public CompositePersonnels delete(Integer id) {
